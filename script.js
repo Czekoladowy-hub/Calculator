db9aaf4212fc59d81d8b8d32ff1cd53f0527db16
Dominik <kontakt.minik@gmail.com>
2023-04-09 19:29:39 +0200

    Create multiply function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function add(number1, number2) {
-  result = number1 + number2;
+  let x = number1 + number2;
 }
 
 function subtract(number1, number2) {
-  result = number1 - number2;
+  let x = number1 - number2;
+}
+
+function multiply(number1, number2) {
+  let x = number1 * number2;
 }
