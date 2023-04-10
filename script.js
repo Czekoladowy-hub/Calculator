108de59c3d7987d82ee12db9e03288c65353b864
Dominik <kontakt.minik@gmail.com>
2023-04-10 17:22:41 +0200

    Repeat case when deviding by 0

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
+const reset = document.querySelector(".reset");
 let screen = document.querySelector(".screen");
 let equal = document.querySelector(".isEqual");
 let screenValue = "";
-let number1;
-let number2;
-let result;
-let sign;
+let number1 = "";
+let number2 = "";
+let result = "";
+let sign = "";
 
 numbers.forEach((btn) =>
   btn.addEventListener("click", (e) => {
     if (screenValue === "") {
       screen.innerHTML = "";
     }
-    console.log(e.target.textContent);
     let input = e.target.textContent;
     screen.innerHTML += input;
     screenValue += input;
@@ -25,24 +25,35 @@ operators.forEach((operator) =>
     if (typeof number1 !== "number") {
       number1 = +screenValue;
       screenValue = "";
-      screen.innerHTML = "";
       sign = e.target.textContent;
+      console.log(number1);
       console.log(sign);
     } else if (typeof number1 === "number") {
-      number2 = +screenValue;
+      if (screenValue !== "") {
+        number2 = +screenValue;
+      } else {
+        return;
+      }
       screenValue = "";
-      screen.innerHTML = "";
       checkSign(sign);
       sign = e.target.textContent;
+      console.log(number2);
+      console.log(`Result is: ${result}`);
     }
   })
 );
 
 equal.addEventListener("click", () => {
-  number2 = +screenValue;
-  screenValue = "";
-  screen.innerHTML = "";
-  checkSign(sign);
+  if (screenValue !== "") {
+    number2 = +screenValue;
+    screenValue = "";
+    screen.innerHTML = "";
+    checkSign(sign);
+    console.log(number2);
+    console.log(`Result is: ${result}`);
+  } else {
+    return;
+  }
 });
 
 function checkSign() {
@@ -59,7 +70,17 @@ function checkSign() {
     screen.innerHTML = devide(number1, number2);
     number1 = devide(number1, number2);
   }
+  sign = "";
 }
+
+reset.addEventListener("click", () => {
+  number1 = "";
+  number2 = "";
+  result = "";
+  sign = "";
+  screenValue = "";
+  screen.innerHTML = "";
+});
 
 function add(number1, number2) {
   return (result = number1 + number2);
@@ -74,5 +95,11 @@ function multiply(number1, number2) {
 }
 
 function devide(number1, number2) {
-  return (result = number1 / number2);
+  if (number2 !== 0) {
+    return (result = number1 / number2);
+  } else if (number2 === "") {
+    return;
+  } else if (number2 === 0) {
+    return "( : NICE TRY";
+  }
 }
